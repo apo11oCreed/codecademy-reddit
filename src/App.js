502519fe42fe70d1b62d984d66004932a07160bb
Nelson Correia <nelson.correia@hdsupply.com>
2022-11-02 01:59:04 -0400

    Created Reddit svg instance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Svgs from './components/svgs';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 20 20">
+        <use href="#redditVector" xlinkHref="#redditVector" />
+      </svg>
       </header>
+      <Svgs />
     </div>
   );
 }
