8b900999d18c97519239a49297e8138b81a7ea96
Nelson Correia <nelson.correia@hdsupply.com>
2022-11-04 01:08:57 -0400

    Stylus added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Svgs from './components/svgs';
-import './App.css';
+import Search from './components/search';
+import './static/css/styles.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <svg className="reddit" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 20 20">
+      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 20 20">
         <use href="#redditVector" xlinkHref="#redditVector" />
       </svg>
+      <Search />
       </header>
       <Svgs />
     </div>
---
+++ src/components/search/index.js
@@ -0,0 +1,9 @@
+const Search = () => {
+    return (
+        <>
+        <h1>This is my Reddit App.</h1>
+        </>
+    )
+}
+
+export default Search;
